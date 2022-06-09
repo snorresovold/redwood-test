42c2b682119ad933371b0d0f9835fdee0fecccba
snorresovold <snorresovold@gmail.com>
2022-06-09 21:30:28 +0200

    Update ContactPage.tsx

--- redwoodblog/web/src/pages/ContactPage/ContactPage.tsx
+++ redwoodblog/web/src/pages/ContactPage/ContactPage.tsx
@@ -1,19 +1,28 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import { Form, Submit, TextAreaField } from '@redwoodjs/forms'
+import { Form, Submit, TextAreaField, FieldError } from '@redwoodjs/forms'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form>
+      <Form onSubmit={onSubmit}>
         <label>name</label>
         <TextAreaField name='name' />
+        <FieldError name="name"/>
+
         <label>email</label>
         <TextAreaField name='email' />
+        <FieldError name="email"/>
+
         <label>message</label>
         <TextAreaField name='message' />
+        <FieldError name="message"/>
+        
         <Submit>Send message</Submit>
       </Form>
     </>
