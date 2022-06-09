491e76ac5ec292a7fee39ac627b6cfc7bdfd7814
snorresovold <snorresovold@gmail.com>
2022-06-09 14:27:44 +0200

    Update ContactPage.tsx

--- redwoodblog/web/src/pages/ContactPage/ContactPage.tsx
+++ redwoodblog/web/src/pages/ContactPage/ContactPage.tsx
@@ -1,19 +1,21 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { Form, Submit, TextAreaField } from '@redwoodjs/forms'
 
 const ContactPage = () => {
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <h1>ContactPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ContactPage/ContactPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>contact</code>, link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
+      <Form>
+        <label>name</label>
+        <TextAreaField name='name' />
+        <label>email</label>
+        <TextAreaField name='email' />
+        <label>message</label>
+        <TextAreaField name='message' />
+        <Submit>Send message</Submit>
+      </Form>
     </>
   )
 }
